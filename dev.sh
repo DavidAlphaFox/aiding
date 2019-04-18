c042ca6b6586efe600d116a885ba7fbe9425bb98
David.Gao <david.alpha.fox@gmail.com>
2019-04-18 18:16:56 +0800

    department message

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
 export ERL_MAX_PORTS=2048
-gmake && cd $(pwd)/examples && erlc *.erl &
+gmake && cd $(pwd)/examples && erlc -I ../include  *.erl &
 wait $!
 exec erl +K true -pa $(pwd)/ebin $(pwd)/examples $(find $(pwd)/deps -type d -name ebin | xargs) -s aiding
