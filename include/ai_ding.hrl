50ac115fce2eca9ea27d5e69a21edfe9ee9f55ec
David.Gao <david.alpha.fox@gmail.com>
2019-04-18 15:06:00 +0800

    add ai_ding_*_msg record

--- include/ai_ding.hrl
+++ include/ai_ding.hrl
@@ -1,2 +1,47 @@
+-record(ai_ding_link_msg,{
+                          message_url,
+                          pic_url,
+                          title,
+                          text
+                         }).
+-record(ai_ding_media_msg,{
+                           media_id,
+                           duration
+                         }).
+-record(ai_ding_oa_msg_header,{
+                               bgcolor,
+                               text
+                              }).
+-record(ai_ding_oa_msg_body,{
+                             title,
+                             form,
+                             rich,
+                             content,
+                             image,
+                             file_count,
+                             author
+                            }).
+-record(ai_ding_oa_msg,{
+                        message_url,
+                        pc_message_url,
+                        header,
+                        body
+                       }).
+-record(ai_ding_markdown_msg,{
+                              title,
+                              text
+                             }).
+-record(ai_ding_action_card_msg,{
+                                 title,
+                                 markdown,
+                                 single_title,
+                                 single_url,
+                                 btn_orientation,
+                                 btn_json_list
+                                }).
+-record(ai_ding_message,{
+                         type,
+                         content
+                        }).
 -define(DING_OAPI_GETTOKEN,<<"dingtalk.oapi.gettoken">>).
 -define(DING_OAPI_MESSAGE_CORP_ASYNC,<<"dingtalk.oapi.message.corpconversation.asyncsend_v2">>).
--- src/ai_ding_message.erl
+++ src/ai_ding_message.erl
@@ -1,5 +1,5 @@
 -module(ai_ding_message).
--export([text/1]).
+-include("ai_ding.hrl").
 -export([send_to_all/2,send_to_departs/3,send_to_users/3,send_to/4]).
 
 send_to_all(AgentID,Message)-> 
@@ -8,7 +8,8 @@ send_to_departs(AgentID,Message,DepartList)->
     send_to(AgentID,Message,DepartList,undefined).
 send_to_users(AgentID,Message,UserList)->
     send_to(AgentID,Message,undefined,UserList).
-send_to(AgentID,Message,DepartList,UserList)->
+send_to(AgentID,Content,DepartList,UserList)->
+    Message = build(Content#ai_ding_message.type,Content#ai_ding_message.content),
     Base = [
             {<<"agent_id">>,AgentID},
             {<<"msg">>,Message}
@@ -25,9 +26,49 @@ send_to(AgentID,Message,DepartList,UserList)->
              {<<"userid_list">>,UserList}
              | Base]
     end.
-text(Content)->
+
+build(text,Content)->
     Content0 = ai_string:to_string(Content),
     [
      {<<"msgtype">>,<<"text">>},
      {<<"text">>,[{<<"content">>,Content0}]}
-     ].
+    ];
+build(image,Content) ->
+    [
+     {<<"msgtype">>,<<"image">>},
+     {<<"image">>,[{<<"media_id">>,
+                    ai_string:to_string(Content#ai_ding_media_msg.media_id)
+                   }]}
+    ];
+build(voice,Content) ->
+    [
+     {<<"msgtype">>,<<"voice">>},
+     {<<"voice">>,[
+                   {<<"media_id">>,
+                    ai_string:to_string(Content#ai_ding_media_msg.media_id)},
+                   {<<"duration">>,
+                    ai_string:to_string(Content#ai_ding_media_msg.duration)}
+                  ]}
+    ];
+build(file,Content) ->
+    [
+     {<<"msgtype">>,<<"file">>},
+     {<<"file">>,[{<<"media_id">>,
+                   ai_string:to_string(Content#ai_ding_media_msg.media_id)
+                  }]}
+    ];
+build(link,Content)->
+    [
+     {<<"msgtype">>,<<"link">>},
+     {<<"link">>,[
+                  {<<"messageUrl">>,
+                   ai_string:to_string(Content#ai_ding_link_msg.message_url)},
+                  {<<"picUrl">>,
+                   ai_string:to_string(Content#ai_ding_link_msg.pic_url)},
+                  {<<"title">>,
+                   ai_string:to_string(Content#ai_ding_link_msg.title)},
+                  {<<"text">>,
+                   ai_string:to_string(Content#ai_ding_link_msg.text)}
+                 ]}
+    ].
+
